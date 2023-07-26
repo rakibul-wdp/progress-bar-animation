558a07657a2ebc0434f6abf56541a26c9cc70827
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-26 18:29:05 +0600

    progress bar value implement in style and done

--- js/script.js
+++ js/script.js
@@ -8,7 +8,12 @@ const animateProgressBar = () => {
       (section.getBoundingClientRect().height -
         document.documentElement.clientHeight)) *
     100;
-  console.log(progressWidth);
+  let value = Math.floor(progressWidth);
+  progressBar.style.width = value + "%";
+
+  if (value < 0) {
+    progressBar.style.width = "0%";
+  }
 };
 
 window.addEventListener("scroll", animateProgressBar);
