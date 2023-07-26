58e7097505fcb66f13194cc3503a93e625983ce6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-07-26 18:23:01 +0600

    get progress width in 100

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ const section = document.querySelector("section");
 
 const animateProgressBar = () => {
   let scrollDistance = -section.getBoundingClientRect().top;
-  console.log(scrollDistance);
+  let progressWidth =
+    (scrollDistance /
+      (section.getBoundingClientRect().height -
+        document.documentElement.clientHeight)) *
+    100;
+  console.log(progressWidth);
 };
 
 window.addEventListener("scroll", animateProgressBar);
